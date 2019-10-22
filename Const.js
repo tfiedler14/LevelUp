54bdcb1ef2fd7b74d9d5dd0d5c86fa2e555218d5
Trixander <trixandera@gmail.com>
2019-10-22 09:35:02 -0500

    firebaseConfig now has up to date information.

--- Const.js
+++ Const.js
@@ -1,9 +1,9 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "key",
-    authDomain: "domain",
-    databaseURL: "url",
+    apiKey: "AIzaSyB8O__d6Iph8zyEUIBTcXSgZkB9vvwUs9o",
+    authDomain: "levelup-10cfc.firebaseapp.com",
+    databaseURL: "https://levelup-10cfc.firebaseio.com",
     storageBucket: "",
 };
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
