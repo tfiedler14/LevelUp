1a2832ab403d601e13b21d92c39f6124accbd312
Trixander <trixandera@gmail.com>
2019-11-23 17:26:30 -0600

    Fixes bug with settings requiring an email change.

--- src/features/Settings.js
+++ src/features/Settings.js
@@ -169,8 +169,10 @@ export const mapDispatchToProps = dispatch => {
 export const validate = values => {
   const errors = {};
 
-  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
+  if (values.email && values.email !== "") {
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
   }
 
   if (values.password && (values.password.length < 6 || values.password.length > 16)) {
