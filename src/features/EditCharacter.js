4ff5ae68d7af336fa271fb4b01c250e7e95902a0
Ian <henscheid@wisc.edu>
2019-12-08 18:04:54 -0600

    Basic layout done, click map to see

--- src/features/EditCharacter.js
+++ src/features/EditCharacter.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useEffect } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { Image, ScrollView, View, Text, ImageBackground } from 'react-native';
+import { Image, ScrollView, View, Text, ImageBackground, Button } from 'react-native';
 import { Field, initialize, reduxForm } from 'redux-form';
 import { WrappedTextInput } from '../shared-components/FormField';
 import { setLocation } from '../logic/location/actions';
@@ -19,52 +19,89 @@ export const EditCharacter = ({ getData, setLocation, location, auth }) => {
   }, []);
   return (
     <View style={styles.sectionHeight}>
-      <View style={styles.sectionPadding}>
+      <View style={styles.nameField}>
         <Text style={{ textAlign: 'center', fontSize: 26, color: 'white', marginTop: 20 }}>
           {'Edit Character'}
         </Text>
+      </View>
         <Field
           name="name"
           id="name"
-          props={{ title: 'State your name peasant' }}
+          props={{ title: 'Choose your name:' }}
           component={WrappedTextInput}
         />
-        <View stlye ={styles.avatarSelect}>
-          <Grid>
-            <Col style={{ width: 20 }}>
-              <Icon
+
+
+      <Grid style={styles.avatarSelect}>
+        <Col style ={styles.arrows}>
+          <View>
+            <Icon style={{paddingRight:10}}
               name="add"
               size={48}
               color="white"
-              />
-            </Col>
-            <Col style={{ width: 40 }}>
-              <Image
-                style={styles.imageProfile}
-                source={require('../../assets/images/waycoolercharacter.png')}
-              />
-            </Col>
-            <Col style={{ width: 20 }}>
-              <Icon
-                name="add"
-                size={48}
-                color="white"
-              />
-            </Col>
-          </Grid>
-        </View>
-      </View>
+            />
+          </View>
+        </Col>
+        <Col style={styles.avatar}>
+          <Image
+            style={styles.imageProfile}
+            source={require('../../assets/images/waycoolercharacter.png')}
+          />
+        </Col>
+        <Col style ={styles.arrows}>
+          <View>
+            <Icon style={{paddingLeft:10}}
+              name="add"
+              size={48}
+              color="white"
+            />
+          </View>
+        </Col>
+      </Grid>
+
+      <Grid>
+        <Col style = {styles.buttons}>
+          <Button
+            color='green'
+            mode= 'contained'
+            title="Confirm Changes"
+            onPress={() => setLocation('profile')}>
+            Confirm Changes
+          </Button>
+        </Col>
+        <Col>
+        </Col>
+        <Col style = {styles.buttons}>
+          <Button
+            color='red'
+            mode='contained'
+            title="Cancel Changes"
+            onPress={() => setLocation('profile')}>
+            Confirm Changes
+          </Button>
+        </Col>
+      </Grid>
+
     </View>
   );
 };
 
 const styles = EStyleSheet.create({
   avatarSelect: {
-    paddingTop: '5rem'
+    paddingTop: '2rem'
+  },
+  arrows: {
+    paddingTop: '5rem',
+    width: '20%'
+  },
+  avatar: {
+    width: '60%',
+    alignItems: 'center'
   },
   nameField: {
-    padding: '20rem'
+    paddingBottom: '1rem'
   },
+
   card: {
     width: '100%',
     height: '5rem',
@@ -78,16 +115,21 @@ const styles = EStyleSheet.create({
     paddingTop: '40%'
   },
   imageProfile: {
-    width: 225,
-    height: 225,
+    width: '14rem',
+    height: '14rem',
     resizeMode: 'stretch'
   },
   sectionHeight: {
     height: '100%',
+    padding: '2rem',
+    paddingTop: '2rem'
   },
   sectionPadding: {
     padding: '2rem',
   },
+  buttons: {
+    width: '40%'
+  }
 
 });
 
