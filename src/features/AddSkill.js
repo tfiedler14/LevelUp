1f7616bb55777ee7962160c23c5cc641bf943520
Tom Fiedler <fiedler.tom14@gmail.com>
2019-12-10 22:49:24 -0600

    redesign of addSkill

--- src/features/AddSkill.js
+++ src/features/AddSkill.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { View, ImageBackground, StyleSheet, Item, Text } from 'react-native';
+import { View, ImageBackground, StyleSheet, Item, ScrollView } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import EStyleSheet, { value } from 'react-native-extended-stylesheet';
 import { setLocation } from '../logic/location/actions';
@@ -20,51 +20,33 @@ export const AddSkill = ({ auth, skills, setLocation, location, handleSubmit, pu
   return (
 
     <ImageBackground source={require('../../assets/images/newBackgroundNoPatterndarker.png')} style={{ height: '100%', width: '100%' }}>
-      <View>
-        <Card style={styles.card}>
-
-          {/* <View style={styles.cardPadding}> */}
-
-          <View style={{ flex: 1, position: 'absolute', top: 0, justifyContent: 'center', alignSelf: 'center', paddingBottom: '0%', paddingTop: '5%' }}>
-            <Text style={styles.pickHeader}> Define an attribute for your new skill!</Text>
-          </View>
-
-          <View style={{ flex: 1, backgroundColor:'#666', position: 'absolute', top: 70, justifyContent: 'center', alignSelf: 'center',  }} >
+      <View style={styles.sectionHeight}>
+        <ScrollView>
+          <Card style={styles.card}>
+            <FormHeader title={'Add Skill'} />
             <Field
               name="addSkillAttribute"
               component={attributeDropDown}
               mode="dropdown"
-              style={pickerStyles}
-            >
-            </Field>
-          </View>
-
-
-
-          <View style={{ flex: 1, position: 'absolute', top: 150, justifyContent: 'center', alignSelf: 'center', width: '80%' }} >
-            <View style={{ position: 'relative'}}>
-              <Text style={styles.pickHeader}>Name your skill!</Text>
-            </View>
+            />
             
             <Field
-                name="addSkillName"
-                id="addSkillName"
-                props={{
-                  textContentType: 'string',
-                }}
-                component={WrappedTextInput}
-              />
-          </View>
+              name="addSkillName"
+              id="addSkillName"
+              props={{ title: 'Skill Name' }}
+              component={WrappedTextInput}
+            />
 
-          {/* </View> */}
-          
-          <View style={{ flex: 1, position: 'absolute', top: 400, justifyContent: 'center', alignSelf: 'center', width: '80%' }} >
+
+
             <Button
-              color="#fff"
+              color="#064f2f"
+              uppercase={false}
               mode="contained"
-              title="Save Skill"
+              style={styles.buttons}
               onPress={handleSubmit(values => {
-                if (values.addSkillAttribute !== null && values.addSkillName !== null) {
+                console.log(values.addSkillAttribute, values.addSkillName);
+                if (values.addSkillAttribute !== undefined && values.addSkillName !== undefined) {
                   let toInsert = {};
                   toInsert["attribute"] = values.addSkillAttribute;
                   toInsert["name"] = values.addSkillName;
@@ -76,13 +58,20 @@ export const AddSkill = ({ auth, skills, setLocation, location, handleSubmit, pu
                 } else {
                   alert("Attribute and Name must be defined");
                 }
-              })}>Save Skill</Button>
+              })}>
+                Save Skill
+            </Button>
 
-          </View>
-
-        </Card>
-
-
+            <Button
+              color="#4f0617"
+              uppercase={false}
+              mode="contained"
+              style={styles.buttons}
+              onPress={() => setLocation('profile')}>
+                Cancel
+            </Button>
+          </Card>
+        </ScrollView>
       </View>
     </ImageBackground >
   );
@@ -90,12 +79,17 @@ export const AddSkill = ({ auth, skills, setLocation, location, handleSubmit, pu
 
 
 
-const attributeDropDown = ({ input: { onChange, value, ...inputProps }, children, ...pickerProps }) => (
+const attributeDropDown = ({ input: { onChange}}) => (
   <RNPickerSelect
     onValueChange={value => onChange(value)}
     useNativeAndroidPickerStyle={true}
     returnKeyType="next"
     enablesReturnKeyAutomatically
+    style={pickerStyles}
+    placeholder={{
+      label: 'Select an Attribute...',
+      color: 'white'
+    }}
     items={[
       { label: 'academics', value: 'academics' },
       { label: 'crafts', value: 'crafts' },
@@ -115,10 +109,13 @@ const pickerStyles = StyleSheet.create({
     fontSize: 22,
     paddingTop: 8,
     paddingHorizontal: 10,
-    paddingBottom: 0,
-    borderColor: 'white',
+    paddingBottom: 8,
+    borderColor: '#6e6e6e',
     borderRadius: 4,
     borderWidth: 2,
+    marginBottom: 10,
+    width: '90%',
+    alignSelf: 'center'
   },
 
 });
@@ -142,8 +139,8 @@ const styles = EStyleSheet.create({
     margin: '1rem',
     color: 'white',
     backgroundColor: '#555',
-    flex: 1, 
-    flexDirection: 'column', 
+    flex: 1,
+    flexDirection: 'column',
     width: '95%'
   },
   padding: {
@@ -165,19 +162,16 @@ const styles = EStyleSheet.create({
   },
 
   buttons: {
-    marginBottom: '2rem'
+    marginBottom: '.5rem',
+    marginTop: '.5 rem'
   },
   topPadding: {
     paddingLeft: '2.5rem',
     paddingTop: '2.5rem'
   },
-  cardPadding: {
-    // padding: '1rem',
-    // margin: '1rem'
-  },
   pickHeader: {
     justifyContent: 'center',
-    alignSelf:'flex-start',
+    alignSelf: 'flex-start',
     position: 'relative',
     fontSize: '1rem',
     color: 'white',
